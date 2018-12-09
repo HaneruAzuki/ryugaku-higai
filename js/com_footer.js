107964f6cdb8243f76e95b05c79fee7b1a74b5e8
HaneruAzuki <masatoshi.who-am-i@hotmail.co.jp>
2018-12-09 19:07:35 +0900

    いろいろ修正

--- js/com_footer.js
+++ js/com_footer.js
@@ -1,3 +1,3 @@
 $(function(){
     $('#com_footer').load('./contents/footer.html');
-})
+});
--- js/topics.js
+++ js/topics.js
@@ -1,20 +1,12 @@
-// トピックスコンテンツロード
-$(function load_content(){
-    for(var i=1; i<=30; i++){
-        var topic_num = ('00' + i).slice(-2);
-        $('#topic-' + topic_num).load('./contents/topics/topics_' + topic_num + '.html');
-    }
-    // コンテンツ箇所までスクロール
-    $(function page_scroll(){
-        $("html,body").animate({scrollTop:$(location.hash).offset().top});
-        console.log($(location.hash).offset().top);
-    });
-});
-
 //PageTopボタン
 $(function(){
     var topBtn=$('#page-top-button');
-    topBtn.hide();
+    
+    if($(this).scrollTop()>160){
+        topBtn.show();
+    }else{
+        topBtn.hide();
+    }
 
     $(window).scroll(function(){
         if($(this).scrollTop()>160){
